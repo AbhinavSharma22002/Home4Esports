808e3f487e42a4a031e7b2778147acad660e7fe2
Abhinav Sharma <abhinavs@cabin4j.com>
2022-12-15 20:14:59 +0530

    Staging Changes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const { initializeApp } = require('firebase-admin/app');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzDS3vRa0bphCvUIPvS-0rTiijvril1DI",
+  authDomain: "commerce-79534.firebaseapp.com",
+  databaseURL: "https://commerce-79534-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "commerce-79534",
+  storageBucket: "commerce-79534.appspot.com",
+  messagingSenderId: "480750464978",
+  appId: "1:480750464978:web:d1b59f13fe70d07bfda20b",
+  measurementId: "G-TZ3JSPQ9E1"
+  };
+
+  
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
 
 const Cors = require("cors");
-
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,8 +27,8 @@ app.use(Cors());
 
 require('dotenv').config({path:__dirname+'/bin/.env'});
 
-//const database = require("./database/connectMongo");
-//database();
+const database = require("./database/connectMongo");
+database();
 
 const indexRouter = require("./routers/index");
 app.use('/',indexRouter);
