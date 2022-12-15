6f7b6d977fcd70c8f2837fb1a537d7bc8ed18c5c
Ajay Pandey <57106804+panditajay73@users.noreply.github.com>
2022-12-15 22:47:00 +0530

    new Admin Dashboard and product mgmt form
    * new changes in section and adding adminpage
    
    * New Section product and admin dashboard add
    
    * New Section product and admin dashboard add
    
    * New Section product and admin dashboard add

---
+++ buyer-file/src/component/section/productmgmt.jsx
@@ -0,0 +1,97 @@
+import { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import Footer from "../layout/footer";
+import Header from "../layout/header";
+import PageHeader from "../layout/pageheader";
+import SocialMedia from "./socialmedia";
+
+const title = "Add Products";
+
+class AddProduct extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            productName: '',
+            productPrice: '',
+            productSize: '',
+            uploadProduct: '',
+            productDescrition: '',
+        };
+    }
+    render() { 
+        return (
+            <Fragment>
+                <div className="login-section padding-top padding-bottom">
+                    <div className=" container">
+                        <div className="account-wrapper">
+                            <h3 className="title">{title}</h3>
+                            <form className="account-form">
+                                <div className="form-group">
+                                    
+                                    <input
+                                        type="text"
+                                        name="productTitle"
+                                        id="item01"
+                                        value={this.state.productName}
+                                        onChange={(e)=>{this.setState({productName: e.target.value});}}
+                                        placeholder="Enter Product Title *"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        name="productPrice"
+                                        id="item02"
+                                        value={this.state.productPrice}
+                                        onChange={(e)=>{this.setState({productPrice: e.target.value});}}
+                                        placeholder="Enter Product Price *"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <select name="productSize"
+                                        id="item03"
+                                        value={this.state.productSize}
+                                        onChange={(e)=>{this.setState({productSize: e.target.value});}}
+                                        placeholder="Select Size. *">
+                                             <option>-Select Size-</option>
+                                         <option value="small">S</option>
+                                        <option value="medium">M</option>
+                                         <option value="long">L</option>
+                                        <option value="extra">XL</option>
+                                </select>
+                                    
+                                </div>
+                                <div className="form-group">
+                                    <input 
+                                        type="file"
+                                        name="uploadProduct"
+                                        id="item04"
+                                        value={this.state.uploadProduct}
+                                        onChange={(e)=>{this.setState({uploadProduct: e.target.value});}}
+                                        placeholder="Please Select Products *"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <textarea
+                                    name="description"
+                                    id="item05"
+                                    value={this.state.productDescrition}
+                                    onChange={(e)=>{this.setState({productDescrition: e.target.value});}}
+                                    placeholder="Enter Product Description here*"
+                                    
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <button className="d-block default-button"><span>Upload Products</span></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
+ 
+export default AddProduct;
