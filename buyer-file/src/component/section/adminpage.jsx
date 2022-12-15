83da3f5e4c06e93a8e51ada3f696a2669d95e3b8
Ajay Pandey <57106804+panditajay73@users.noreply.github.com>
2022-12-15 20:28:19 +0530

    new changes in section and adding adminpage

--- buyer-file/src/component/section/adminpage.jsx
+++ buyer-file/src/component/section/adminpage.jsx
@@ -1,26 +1,24 @@
 import { Component, useState } from "react";
 import { Link } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Rating from "./rating";
+import ProductMgmt from "../../pages/productmgmt";
 
 
 let AdminPageData = [
     {
         id: 1,
         image: 'assets/images/game/01.png',
-        alt: 'game list name',
-        title: 'Witch Sports Team',
-        icon: [
-            {
-                iconName: 'icofont-star',
-            },
-        ],
+        alt: 'Product Management',
+        title: '     Products Mgmt',
         catagory: 'collection-one',
     },
     {
         id: 2,
         image: 'assets/images/game/02.png',
-        alt: 'game list name',
-        title: 'Witch Sports Team',
+        alt: 'Match Management',
+        title: 'Matches Mgmt',
         catagory: 'collection-two',
     },
 ]
@@ -41,10 +39,9 @@ const AdminPage = () => {
             <div className="container">
                 <div className="section-wrapper">
                     <ul className="collection-filter-button-group common-filter-button-group d-flex flex-wrap justify-content-center mb-5 text-uppercase">  
-                        <li onClick={() => setItems(AdminPageData) }>All matches</li>
-                        <li onClick={() => filterItem('collection-one') }>today’s matches</li>
-                        <li onClick={() => filterItem('collection-two') }>upcoming matches</li>
-                        <li onClick={() => filterItem('collection-three') }>matche results</li>
+                        <li onClick={() => setItems(AdminPageData) }>All Managements</li>
+                        <li onClick={() => filterItem('collection-one') }>Product Management</li>
+                        <li onClick={() => filterItem('collection-two') }>Matches Management</li>
                     </ul>
                     <div className="row g-4 justify-content-center collection-grid  GameListStyle">
                         {
@@ -52,27 +49,27 @@ const AdminPage = () => {
                             const { id, image, title, alt } = elem;
                                 return (
                                     <div className="col-lg-3 col-md-4 col-sm-6 col-12" key={id}>
-                                        <div className="game-item item-layer">
+                                        
                                             <div className="game-item-inner">
                                                 <div className="game-thumb">
-                                                    <img src={image} alt={alt} />
+                                                <center> <img src={image} alt={alt} /></center>
                                                 </div>
                                                 <div className="game-overlay">
-                                                    <h4><Link to="/team-single">{title}</Link> </h4>
-                                                    <Rating />
+                                                    <center><h4><Link to={ProductMgmt}>{title}</Link> </h4></center>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                               
                                 )
                             })
                         }
                     </div>
                 </div>
             </div>
+            
         </section>
+
     );
 }
- 
-//export default AdminPage;
+ export default AdminPage;
 
