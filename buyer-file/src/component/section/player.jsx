6f9eb47a42955b5953d858a66b9c2a486ab26e50
panditajay73 <ajaypandey91700@gmail.com>
2022-12-30 00:31:55 +0530

    Staging Changes

--- buyer-file/src/component/section/player.jsx
+++ buyer-file/src/component/section/player.jsx
@@ -1,107 +1,115 @@
-import { Component } from "react";
+import { Fragment,useEffect,useState } from "react";
 import { Link } from "react-router-dom";
 import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SocialMedia from "./socialmedia";
 
-const subtitle = "Our Team Players";
-const title = "Meet Our Squad Players ";
+const subtitle = "Our Teams";
+const title = "Meet Our Squads ";
 
 
-let PlayerListContent = [
-    {
-        bgImgUrl: 'assets/images/team/1.jpg',
-        imgUrl: 'assets/images/team/1.png',
-        imgAlt: 'Player Thumb',
-        title: 'Dragon Team',
-    },
-    {
-        bgImgUrl: 'assets/images/team/2.jpg',
-        imgUrl: 'assets/images/team/2.png',
-        imgAlt: 'Player Thumb',
-        title: 'Frogs Team',
-    },
-    {
-        bgImgUrl: 'assets/images/team/3.jpg',
-        imgUrl: 'assets/images/team/3.png',
-        imgAlt: 'Player Thumb',
-        title: 'Tigers Team',
-    },
-    {
-        bgImgUrl: 'assets/images/team/4.jpg',
-        imgUrl: 'assets/images/team/4.png',
-        imgAlt: 'Player Thumb',
-        title: 'Shark Team',
-    },
-    {
-        bgImgUrl: 'assets/images/team/5.jpg',
-        imgUrl: 'assets/images/team/5.png',
-        imgAlt: 'Player Thumb',
-        title: 'Gaming Team',
-    },
-]
+const PlayerSection = (props)=>{
+    const [PlayerList,setPlayerList] = useState([]);
 
-class PlayerSection extends Component {
-    render() { 
-        return (
-            <section className="player-section padding-top padding-bottom">
-                <div className="container-fluid">
-                    <div className="section-header">
-                        <p>{subtitle}</p>
-                        <h2>{title}</h2>
-                    </div>
-                    <div className="section-wrapper">
-                        <div className="player-slider">
-                            <Swiper
-                                spaceBetween={20}
-                                slidesPerView={2}
-                                loop={'true'}
-                                autoplay={{
-                                    delay: 5000,
-                                    disableOnInteraction: false,
-                                }}
-                                modules={[Autoplay]}
-                                breakpoints={{
-                                    0: {
-                                        width: 0,
-                                        slidesPerView: 1,
-                                    },
-                                    768: {
-                                        width: 768,
-                                        slidesPerView: 2,
-                                    },
-                                    1200: {
-                                        width: 1200,
-                                        slidesPerView: 3,
-                                    },
-                                }}
-                            >
-                                {PlayerListContent.map((val, i) => (
-                                    <SwiperSlide key={i}>
-                                        <div className="player-item-2 text-center" style={{backgroundImage: `url(${val.bgImgUrl})`}}>
-                                            <div className="player-inner-2">
-                                                <div className="player-thumb-2">
-                                                    <Link to="/team-single">
-                                                        <img src={`${val.imgUrl}`} alt={`${val.imgAlt}`} />
-                                                    </Link>
-                                                </div>
-                                                <div className="player-content-2">
-                                                    <Link to="/team-single"><h2>{val.title}</h2></Link>
-                                                    <ul className="match-social-list d-flex flex-wrap align-items-center justify-content-center">
-                                                        <SocialMedia />
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        );
-    }
-}
- 
+    // function getID(){
+    //     let pair = window.location.search.substring(1).split("=");
+    //     return pair[1];
+    // }
+    useEffect(()=>{
+        const someFunc = async (props)=>{
+            let array = [];
+                const requestOptions = {
+                method: "GET"
+            };
+            const response = await fetch(
+                `http://localhost:3001/api/team/getAll`,
+                requestOptions
+            )
+           
+            if(response.status===200){
+                const data = await response.json();
+                console.log(data);
+
+                for(let i = 0;i<data.teams.length;i++){
+                    array.push({
+                        img: `${data.teams[i].image}`,
+                        name: `${data.teams[i].teamName}`,
+                        Date: `${data.teams[i].date}`,
+                        to: `${data.teams[i].team._id}`
+                    });
+                }
+            }
+            setPlayerList(array);
+    };
+            someFunc();
+    },[]);
+
+
+//   array.push({
+//         image: `${data[i].image}`,
+//         teamName: `${data[i].teamName}`,
+//         author: `${data[i].author}`,
+//         date: `${data[i].date}`,
+//         to: `${data[i]._id}`
+//     });
+return(
+    <>
+    <Fragment>
+    <div className="container-fluid">
+                   <div className="section-header">
+                       <p>{subtitle}</p>
+                       <h2>{title}</h2>
+                   </div>
+                   <div className="section-wrapper">
+                       <div className="player-slider">
+                           <Swiper
+                               spaceBetween={20}
+                               slidesPerView={2}
+                               loop={'true'}
+                               autoplay={{
+                                   delay: 5000,
+                                   disableOnInteraction: false,
+                               }}
+                               modules={[Autoplay]}
+                               breakpoints={{
+                                   0: {
+                                       width: 0,
+                                       slidesPerView: 1,
+                                   },
+                                   768: {
+                                       width: 768,
+                                       slidesPerView: 2,
+                                   },
+                                   1200: {
+                                       width: 1200,
+                                       slidesPerView: 3,
+                                   },
+                               }}
+                           >
+                               {PlayerList.map((val, i) => (
+                                   <SwiperSlide key={i}>
+                                       <div className="player-item-2 text-center" style={{backgroundImage: `url({${val.image}})`}}>
+                                           <div className="player-inner-2">
+                                               <div className="player-thumb-2">
+                                                   <Link to="/">
+                                                       <img src={val.image} />
+                                                   </Link>
+                                               </div>
+                                               <div className="player-content-2">
+                                                   <Link to="/"><h2>{val.teamName}</h2></Link>
+                                                   <h2>Date: {val.date}</h2>
+                                                   <h2>Tier: {val.tier}</h2>
+                                               </div>
+                                           </div>
+                                       </div>
+                                   </SwiperSlide>
+                               ))}
+                           </Swiper>
+                       </div>
+                   </div>
+               </div>
+    </Fragment>
+    </>
+
+   );
+};
 export default PlayerSection;
