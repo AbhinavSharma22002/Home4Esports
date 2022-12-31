66497aa6e7527aed793681a140f2c2a82eed8bca
Abhinav Sharma <abhinavs@cabin4j.com>
2022-12-31 16:15:41 +0530

    Staging aws changes.

--- backend/routers/image.js
+++ backend/routers/image.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const fs  = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
 const router = express.Router();
 const multer = require('multer');
 let upload = multer({dest: 'uploads/'});
 
-router.post("/upload",upload.single('image'),(req,res)=>{
-    res.status(200).json({val: "assets/images/blog/single/03.jpg"});
+const {uploadFile,getFileStream} = require('../s3');
+
+router.post("/upload",upload.single('image'),async (req,res)=>{
+    const file = req.file;
+    const result = await uploadFile(file);
+    await unlinkFile(file.path);
+    res.status(200).json({val: `/images/${result.Key}`});
 });
+router.get("/:key",(req,res)=>{
+    const key = req.params.key;
+    const readStream = getFileStream(key);
+    readStream.pipe(res);
+})
 
 module.exports = router;
---
+++ backend/s3.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const fs = require('fs');
+const S3 = require('@aws-sdk/client-s3');
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+});
+
+//uploads a file
+function uploadFile(file){
+    const fileStream = fs.createReadStream(file.path);
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename
+    }
+
+    return s3.upload(uploadParams).promise()
+}
+exports.uploadFile = uploadFile;
+//downloads a file
+function getFileStream(fileKey){
+    const downloadParams = {
+        Key: fileKey,
+        Bucket: bucketName
+    }
+    return s3.getObject(downloadParams).createReadStream();
+}
+exports.getFileStream = getFileStream;
