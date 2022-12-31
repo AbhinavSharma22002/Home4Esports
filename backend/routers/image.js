422edc040dc80e62aeac9dae1a64a9e26105f04c
Abhinav Sharma <abhinavs@cabin4j.com>
2022-12-31 18:58:13 +0530

    Staging s3 changes.

--- backend/routers/image.js
+++ backend/routers/image.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const fs  = require('fs');
 const util = require('util');
 const unlinkFile = util.promisify(fs.unlink);
@@ -12,7 +13,7 @@ router.post("/upload",upload.single('image'),async (req,res)=>{
     const file = req.file;
     const result = await uploadFile(file);
     await unlinkFile(file.path);
-    res.status(200).json({val: `/images/${result.Key}`});
+    res.status(200).json({val: `${process.env.BASE}/aws/image/${result.Key}`});
 });
 router.get("/:key",(req,res)=>{
     const key = req.params.key;
