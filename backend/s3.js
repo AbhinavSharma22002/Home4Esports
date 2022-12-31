8f884fe595b8ff695da09e5e0b5e589b5cd81527
Abhinav Sharma <abhinavs@cabin4j.com>
2022-12-31 16:22:35 +0530

    Staging Changes.

--- backend/s3.js
+++ backend/s3.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
-const S3 = require('@aws-sdk/client-s3');
+const S3 = require('aws-sdk/clients/s3');
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY;
