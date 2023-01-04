4e7f4867f50eec33b89acaddd8951ad50869e2ab
panditajay73 <ajaypandey91700@gmail.com>
2023-01-04 13:00:54 +0530

    Staging Changes

--- buyer-file/src/component/section/testimonial.jsx
+++ buyer-file/src/component/section/testimonial.jsx
@@ -3,34 +3,49 @@ import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Rating from "./rating";
 
-const subtitle = "our Founders";
-const title = "Our Founder & Co-Founders";
+const subtitle = "our reviews";
+const title = "Our Testimonials";
 
 let TestimonialListContent = [
     {
         imgUrl: 'assets/images/testimonial/01.jpg',
         imgAlt: 'testimonial thumb',
-        title: 'Peeyush "PsyLens" Kumar',
-        desc: 'Ex-Microsoft, Esports Mentor India Accelerator, Speaker, Caster Esports (15+ Yrs),Corporates (8 Yrs)',
-        dige: 'Founder of CCE',
+        title: 'INSTINCTs',
+        desc: 'We love this event, looking forword to enjoy upcoming event with better time management.',
+        dige: 'Second (2nd)',
         ratingcount: '5.28',
     },
     {
-        imgUrl: 'assets/images/testimonial/03.jpg',
+        imgUrl: 'assets/images/testimonial/01.jpg',
         imgAlt: 'testimonial thumb',
-
-        title: 'Samhita "SheSapien"',
-        desc: 'ISB, SEEDS Board Member, Ex-Microsoft, Honeywell, EY-Esports (2+ Yrs), Corporates (15+ Yrs)',
-        dige: 'Co-Founder of CCE',
-        ratingcount: '5.80',
+        title: 'AlphaQ',
+        desc: 'Was good enough, maybe streaming can get better as we are unable to view a few past games. ',
+        dige: 'First (1st)',
+        ratingcount: '5.28',
     },
     {
-        imgUrl: 'assets/images/testimonial/02.jpg',
+        imgUrl: 'assets/images/testimonial/01.jpg',
         imgAlt: 'testimonial thumb',
-        title: 'Pravesh "EvilEvr" Rawat',
-        desc: 'Creative Enterpreneur, Pro Gamer, Artist Esports (10+Yrs) ,Corporates (2 Yrs)',
-        dige: 'Co-Founder of CCE',
-        ratingcount: '5.80',
+        title: 'Satanic',
+        desc: 'We do not face any time issue but others do so if possible then 100% of the audience will love your tournaments',
+        dige: 'Second (2nd)',
+        ratingcount: '5.28',
+    },
+    {
+        imgUrl: 'assets/images/testimonial/01.jpg',
+        imgAlt: 'testimonial thumb',
+        title: 'Nirvana Esports',
+        desc: 'Include Winner bracket Looser bracket format, its interesting.',
+        dige: 'Fourth (4th)',
+        ratingcount: '5.28',
+    },
+    {
+        imgUrl: 'assets/images/testimonial/01.jpg',
+        imgAlt: 'testimonial thumb',
+        title: 'OMEN eSports ',
+        desc: 'Nice tourney no issues from our side ',
+        dige: 'Third (3rd)',
+        ratingcount: '5.28',
     },
 ]
 
@@ -71,13 +86,11 @@ class TestimonialSection extends Component {
                             >
                                 {TestimonialListContent.map((val, i) => (
                                     <SwiperSlide key={i}>
-                                        <div className="testimonial-item">
+                                                <div className="testimonial-item">
                                             <div className="testimonial-inner">
                                                 <div className="testimonial-head">
                                                     <div className="testi-top">
-                                                        <div className="testimonial-thumb">
-                                                            <img src={`${val.imgUrl}`} alt={`${val.imgAlt}`} />
-                                                        </div>
+                                                       
                                                         <div className="name-des">
                                                             <h5>{val.title}</h5>
                                                             <p>{val.dige}</p>
@@ -86,6 +99,9 @@ class TestimonialSection extends Component {
                                                 </div>
                                                 <div className="testimonial-body">
                                                     <p>{val.desc}</p>
+                                                </div>
+                                                <div className="testimonial-footer">
+                                                    <Rating />
                                                 </div>
                                             </div>
                                         </div>
